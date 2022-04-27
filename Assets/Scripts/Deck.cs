3d42d5f0671dddbb3d70c833df74f6d663150a18
danialfaro <infdalfamosni@gmail.com>
2022-04-27 12:41:15 +0200

    Implementado ShuffleCards

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -55,6 +55,12 @@ public class Deck : MonoBehaviour
          * El método Random.Range(0,n), devuelve un valor entre 0 y n-1
          * Si lo necesitas, puedes definir nuevos arrays.
          */
+        for (int i = 0; i < values.Length; i++)
+        {
+            int newIndex = UnityEngine.Random.Range(0, values.Length);
+            values[i] = values[newIndex];
+            faces[i] = faces[newIndex];
+        }
     }
 
     void StartGame()
